9c970996fe1387e765326ab77e52f1be753d505c
TaldoPlinio <Taldojovic@gmail.com>
2024-06-08 23:34:33 -0300

    Add files via upload

---
+++ src/js/index.js
@@ -0,0 +1,64 @@
+alert('Sejam bem vindos')
+/*
+OBJETIVO 1 - quando clicarmos na seta de anvaçar temos que mostrar o proximo cartão da lista
+- passo 1 - dar um jeito de pegar o elemento HTML da seta avancar
+- passo 2 - dar um jeito de identificar o clique do usuário cartão da lista
+- passo 3 - fazer aparecer o próximo cartão da lista
+- passo 4 - buscar o cartão que esta selecionado e esconder
+
+OBJETIVO 2 - quando clicarmos na seta de voltar temos que mostrar o cartão anterior da lista
+- passo 1 - dar um jeito de pegar o elemento HTML da seta voltar
+- passo 2 - dar um jeito de identificar o clique do usuário na seta voltar
+- passo 3 - fazer aparecer o cartão anterior da lista
+- passo 4 - buscar o cartão que está selecionado e esconder
+*/
+
+// - passo 1 - dar um jeito de pegar o elemento HTML da seta avancar
+const btnAvancar = document.getElementById("btn-avancar");
+
+//- passo 1 - dar um jeito de pegar o elemento HTML da seta voltar
+const btnVoltar = document.getElementById("btn-voltar")
+//---------------------------------------------------------------------------
+let cartaoAtual = 0;
+const cartoes = document.querySelectorAll(".cartao");
+
+//- passo 2 - dar um jeito de identificar o clique do usuário cartão da lista
+btnAvancar.addEventListener("click", function () {
+
+const ehUltimoCartao = cartaoAtual === cartoes.length - 1
+    if (ehUltimoCartao) return
+
+    //- passo 4 - buscar o cartão que esta selecionado e esconder
+    esconderCartaoSelecionado();
+
+    //- passo 3 - fazer aparecer o próximo cartão da lista, colocando a classe selecionado nele
+
+    cartaoAtual++;
+    mostrarCartao();
+
+});
+
+//- passo 2 - dar um jeito de identificar o clique do usuário na seta voltar
+btnVoltar.addEventListener("click", function () {
+
+    const ehPrimeiroCartao = cartaoAtual === 0
+    if (ehPrimeiroCartao) return;
+
+    esconderCartaoSelecionado();
+
+    //    - passo 3 - fazer aparecer o cartão anterior da lista
+    cartaoAtual--;
+    mostrarCartao();
+
+
+})
+
+function mostrarCartao() {
+    cartoes[cartaoAtual].classList.add("selecionado");
+}
+
+function esconderCartaoSelecionado() {
+    const cartaoSelecionado = document.querySelector(".selecionado");
+    cartaoSelecionado.classList.remove("selecionado");
+}
+
